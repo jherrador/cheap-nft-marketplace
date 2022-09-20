9e2d6e8e4d365106cb9aaea42dfabb98cbe3e9c0
Javier Herrador Cuartero <qherra@gmail.com>
2022-09-20 15:36:01 +0200

    Update eslint

--- app.js
+++ app.js
@@ -1,35 +1,37 @@
-require("dotenv").config();
+require('dotenv').config()
 
-const express = require("express");
-const morgan = require("morgan");
+const express = require('express')
+const morgan = require('morgan')
 
-const app = express();
+const app = express()
 
-const listedNfts = [];
+const listedNfts = []
 // Settings
-app.set("views", "./static/views");
-app.set("view engine", "ejs");
-app.set("appName", process.env.APP_NAME);
-app.set("port", process.env.PORT);
-app.set("listedNfts", listedNfts);
+app.set('views', './static/views')
+app.set('view engine', 'ejs')
+app.set('appName', process.env.APP_NAME)
+app.set('port', process.env.PORT)
+app.set('listedNfts', listedNfts)
 
 // MiddleWares
-app.use(express.json());
-app.use(express.static("static"));
-app.use(morgan("dev"));
+app.use(express.json())
+app.use(express.static('static'))
+app.use(morgan('dev'))
 
 // Routes
-const routes = require("./routes/auction.routes");
+const routes = require('./routes/auction.routes')
 
-app.use(routes);
+app.use(routes)
 
 app.use((req, res) => {
-  res.status(404);
-  if (req.accepts("html")) {
-    res.render("pages/index", { url: req.url });
-  }
-});
+    res.status(404)
+    if (req.accepts('html')) {
+        res.render('pages/index', { url: req.url })
+    }
+})
 
 app.listen(3000, () => {
-  console.log(`${app.get("appName")} application running on port ${app.get("port")}`);
-});
+    console.log(
+        `${app.get('appName')} application running on port ${app.get('port')}`
+    )
+})
